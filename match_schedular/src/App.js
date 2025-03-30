6d4c3000d6227a3d87bfb9f82a4053e31df71163
Abhirup Datta Khan <141467746+abhirup0199@users.noreply.github.com>
2025-03-30 13:46:42 +0000

    3

--- match_schedular/src/App.js
+++ match_schedular/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
+import { HashRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
 import League from "./pages/League";
 import RoundRobin from "./pages/RoundRobin";
 import Knockout from "./pages/Knockout";
@@ -9,7 +9,7 @@ import "./App.css";
 
 const App = () => {
     return (
-        <Router basename="/">
+        <Router>
             <Main />
         </Router>
     );
